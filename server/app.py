9120f749ac2726586799f92e05dbb3cf7177edf1
Eddah kibet <eddahkibet20@gmail.com>
2025-09-09 20:41:43 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -5,4 +5,34 @@ from flask import Flask
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_hello(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<parameter>')
+def count(parameter):
+    return '\n'.join(str(i) for i in range(int(parameter))) + '\n'
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math_operation(num1, operation, num2):
+    num1, num2 = int(num1), int(num2)
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == '/':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        return 'Invalid operation'
+    
